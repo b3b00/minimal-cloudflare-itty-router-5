f5fa355a52415d3eeb64c9f054cdcf2d694489b1
b3b00 <olivier.duhart@gmail.com>
2024-04-02 16:18:05 +0200

    initial

---
+++ public/_worker.ts
@@ -0,0 +1,14 @@
+import { Router, AutoRouter, IttyRouter, IRequest } from 'itty-router'
+
+const router = AutoRouter()
+
+router.get('/test',(request : IRequest) => {
+    return {'result':'OK'};
+});
+
+router.all('*',  (request:IRequest, env:any) => {
+    console.log(`assets handler ${request.url} ${request.path}`)
+    return env.ASSETS.fetch(request.url)
+});
+
+export default { ...router } // this looks pointless, but trust us
